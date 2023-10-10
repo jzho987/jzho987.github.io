0428f506e9b669467f229e365219436cdd089d72
jzho987 <79814775+jzho987@users.noreply.github.com>
2023-10-10 13:21:01 +1300

    feat: redid menu bar

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -2,30 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import MenuBar from "./Components/MenuBar/MenuBar";
-import { Layout, Menu, Breadcrumb } from 'antd';
 import HeaderBar from "./Components/Header/HeaderBar";
 import HomePage from "./Pages/HomePage/HomePage";
-
-
-const { Header, Content, Sider, Footer } = Layout;
 
 function App() {
   return (
     <div className="App">
-      <Layout>
-          <Header>
-              <HeaderBar></HeaderBar>
-          </Header>
-          <Layout>
-              <Sider width={250}>
-                  <MenuBar></MenuBar>
-              </Sider>
-              <Content>
-                  <HomePage/>
-              </Content>
-          </Layout>
-          <Footer></Footer>
-      </Layout>
+      <MenuBar/>
+      <HomePage/>
     </div>
   );
 }
--- src/src/Pages/HomePage/HomePage.tsx
+++ src/src/Pages/HomePage/HomePage.tsx
@@ -1,22 +1,13 @@
-import {Space, Typography} from "antd";
-import Title from "antd/es/typography/Title";
-import Paragraph from "antd/es/typography/Paragraph";
+import {Box, Grid} from "@mui/material";
 
 export default function HomePage() {
     return(
-        <div>
-            <Typography>
-                <Title>
-                    Welcome to my space
-                </Title>
-                <Paragraph>
-                    My name is Jay Zhou. Currently a penultimate Software Engineering student at the University of Auckland, here is my journey...
-                </Paragraph>
-                <Title level={2}>
-                    Portfolio Highlights
-                </Title>
-            </Typography>
+        <Box>
+            <Grid container spacing={3}>
+                <Grid item xs={8}>
+                </Grid>
+            </Grid>
 
-        </div>
+        </Box>
     )
 }
