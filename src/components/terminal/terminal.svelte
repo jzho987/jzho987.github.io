05c45726946664b99e623b390668457a0a870a1e
J.Z. Zhou <79814775+jzho987@users.noreply.github.com>
2024-08-03 21:44:59 +1200

    add termianl

---
+++ src/components/terminal/terminal.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import MockMacActionButtons from "./mock_mac_action_buttons.svelte";
+
+    export let name;
+    export let user;
+    export let dir;
+</script>
+
+<div class="flex flex-col flex-1">
+    <div class="rounded-t-lg bg-stone-200 p-2 flex flex-row items-center">
+        <MockMacActionButtons />
+        <div class="flex flex-1 justify-center">
+            <p class="text-sm text-gray-800">{user}@{name}:{dir}</p>
+        </div>
+    </div>
+    <div class="flex-1 rounded-b-lg bg-stone-800 p-3">
+        <p class="text-sm font-extralight text-stone-300">{user} at {name} in {dir}</p>
+    </div>
+</div>
--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
+	import Terminal from "../../components/terminal/terminal.svelte";
+
 </script>
 
-<div class="pt-6 pl-9">
-	<div class="flex flex-col gap-2">
+<div class="px-6 py-9 flex h-screen">
+	<div class="flex flex-col flex-1">
 		<h1 class="font-extralight text-stone-200 text-5xl">Jay Zhou</h1>
 		<h1 class="font-extralight text-stone-200 text-2xl">Master's of Computer Science</h1>
 	</div>
-
+	<div class="flex flex-1">
+		<Terminal 
+			user="jayzhou"
+			name="JayZhou-Website"
+			dir="~/Desktop"
+		/>
+	</div>
 </div>
